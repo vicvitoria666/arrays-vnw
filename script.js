90eed774ad157ba93a989f7cada7b11582ddbb0b
vicvitoria666 <https.vitoriaoliveira@gmail.com>
2022-02-16 17:26:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+ // Crie um array que receba 5 itens e exiba no console.
+
+let makeup = ["foundation","eyeliner", "lipstick","lipbalm","eyeshadow"] 
+    console.log(makeup)
+
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+makeup.unshift('blush')
+
+// Utilize um método para remover o último nome do array.
+
+makeup.pop('eyeshadow')
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+makeup.push('mascara', 'powder')
+
+// Utilize um método para remover o primeiro nome do array.
+
+makeup.shift()
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+
+console.log(numbers)
